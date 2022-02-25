cae2b1130cc021a787a8b22f765894886fc69233
edsonsabino <100230731+edsonsabino@users.noreply.github.com>
2022-02-25 10:06:33 -0300

    mais perguntas
    adicionei perguntas ao main

--- __main__.py
+++ __main__.py
@@ -8,6 +8,8 @@ print("main funcinando\n")
 
 nome_do_pet=input("Qual o nome do pet? \n")
 nome_do_dono=input("Qual o seu nome? \n")
-raca_do_dono=input("Qual e raça do seu pet? \n")
+raca_do_pet=input("Qual e raça do seu pet? \n")
 
 print("Nome: ", nome_do_pet)
+print("Nome dono: ", nome_do_dono)
+print("Raca do pet: ", raca_do_pet)
